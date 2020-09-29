9a4173a45c9fda67cca3253e1a915d8322a0cefd
DW <retimes@hotmail.com>
2020-09-29 11:37:33 -0400

    sehgal.py custom

--- orphics/sehgal.py
+++ orphics/sehgal.py
@@ -2,7 +2,7 @@ import healpy as hp
 import numpy as np
 import os,sys
 from past.utils import old_div
-from pixell import enmap
+from pixell import enmap, utils
 
 default_tcmb = 2.726
 H_CGS = 6.62608e-27
@@ -18,6 +18,13 @@ def fnu(nu,tcmb=default_tcmb):
     mu = H_CGS*(1e9*nu)/(K_CGS*tcmb)
     ans = mu/np.tanh(old_div(mu,2.0)) - 4.0
     return ans
+
+def jysr2thermo(nu, tcmb=default_tcmb):
+    nu = np.asarray(nu)*1e9
+    mu = H_CGS*(nu)/(K_CGS*tcmb)
+    conv_fact = 2*(K_CGS*tcmb)**3/(H_CGS**2*C_light**2)*mu**4/(4*(np.sinh(mu/2.))**2)
+    conv_fact *= 1e23
+    return 1/conv_fact*tcmb*1e6
 
 class SehgalSky(object):
     def __init__(self,path=None,healpix=True): 
@@ -106,20 +113,31 @@ class SehgalSky(object):
 
 class SehgalSky2010(object):
     # to handle orignal Sehgal et al sims
-    def __init__(self,path=None, data_type="healpix"): 
+    def __init__(self,path=None, data_type="healpix", unit="thermo"): 
         if path is None: path = os.environ['SEHGAL_SKY']
         assert(data_type in ['healpix', 'enmap', 'alm'])
         self.data_type = data_type
         self.path        = path
+        self.unit        = unit
+        assert(self.unit in ["thermo", "jysr"])
         self.frequencies = [str(s).zfill(3) for s in [30,90,148,219,277,350]]
         if self.data_type == 'healpix' :
             self.rfunc = hp.read_map
             self.nside = 8192
         elif self.data_type == 'enmap':
             self.rfunc = enmap.read_map
-            self.shape,self.wcs = enmap.read_map_geometry(self.get_lensed_cmb(True))
+            self.shape,self.wcs = enmap.fullsky_geometry(res=0.5*utils.arcmin)
         elif self.data_type == 'alm':
             self.rfunc = lambda x : hp.read_alm(x, hdu=1)
+
+    def __get_unit_conv(self, freq):
+        ## mistake here healpix in jysr while others in thermo
+        ret = None
+        if self.data_type == "healpix":
+            ret = 1. if self.unit == "jysr" else jysr2thermo(freq)
+        else:
+            ret = 1. if self.unit == "thermo" else 1./jysr2thermo(freq)
+        return ret
 
     def __format_fname(self, fname_temp, freq=None):
         if freq is not None:
@@ -131,34 +149,35 @@ class SehgalSky2010(object):
 
     def get_total_cmb(self,freq,filename_only=False):
         filename = self.__format_fname('{}_skymap_{}.fits', freq=freq)
-        return filename if filename_only else self.rfunc(filename)
+        return filename if filename_only else self.rfunc(filename) * self.__get_unit_conv(freq)
 
     def get_lensed_cmb(self,freq, filename_only=False):
         filename = self.__format_fname('{}_lensedcmb_{}.fits', freq=freq)
-        return filename if filename_only else self.rfunc(filename)
+        return filename if filename_only else self.rfunc(filename) * self.__get_unit_conv(freq)
 
     def get_ksz(self, freq, filename_only=False): 
         filename = self.__format_fname('{}_ksz_{}.fits', freq=freq)
+        return filename if filename_only else self.rfunc(filename) * self.__get_unit_conv(freq)
+
+    def get_kappa(self,filename_only=False): 
+        filename = self.__format_fname('kappa_{}.fits', freq=None)
         return filename if filename_only else self.rfunc(filename)
 
-    def get_kappa(self,filename_only=False):
-        raise NotImplemented("soon")
-
     def get_compton_y(self, tcmb=default_tcmb, scale=0.75):
-        return self.get_tsz(148, scale)/(fnu(148)*tcmb * 1e6)
+        return self.get_tsz(148, scale)/(fnu(148)*tcmb * 1e6) * self.__get_unit_conv(freq)
 
     def get_cib(self,freq,filename_only=False, scale=0.75):
         filename = self.__format_fname('{}_ir_pts_{}.fits', freq=freq)
-        return filename if filename_only else self.rfunc(filename)*scale
+        return filename if filename_only else self.rfunc(filename)*scale * self.__get_unit_conv(freq)
 
     def get_radio(self,freq,filename_only=False):
         filename = self.__format_fname('{}_rad_pts_{}.fits', freq=freq)
-        return filename if filename_only else self.rfunc(filename)
+        return filename if filename_only else self.rfunc(filename) * self.__get_unit_conv(freq)
 
     def get_galactic_dust(self,freq,filename_only=False): 
         filename = self.__format_fname('{}_dust_{}.fits', freq=freq)
-        return filename if filename_only else self.rfunc(filename)
+        return filename if filename_only else self.rfunc(filename) * self.__get_unit_conv(freq)
 
     def get_tsz(self,freq,filename_only=False, scale=0.75):
         filename = self.__format_fname('{}_tsz_{}.fits', freq=freq)
-        return filename if filename_only else self.rfunc(filename)*scale
+        return filename if filename_only else self.rfunc(filename)*scale * self.__get_unit_conv(freq)
